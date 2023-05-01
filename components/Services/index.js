90d16c93476d0f87dd5294f726ee6c7d65b24946
Faxriddin747 <faxriddinradjapov1@gmail.com>
2023-05-01 23:13:49 +0500

    изменения в услугах
    Co-Authored-By: Salimbek21 <82567966+Salimbek21@users.noreply.github.com>

--- components/Services/index.js
+++ components/Services/index.js
@@ -2,31 +2,28 @@ import React from "react";
 import cls from "./services.module.scss";
 import { serviceData } from "@/shared/services";
 
+
 const Services = () => {
   return (
     <>
       <div className="innerContainer">
-        <h1>Услуги доставки из Турции от Modagram.uz</h1>
+        <h1 className={cls.h1}>Услуги доставки из Турции от Modagram.uz</h1>
 
         <div className={cls.services}>
-          <div>
-            {serviceData.map((item) => (
-              <div key={item.id}  className={cls.servicesDelivery}>
-                <div className={cls.servicesName}>
-                  {item.name}
-                  {item.title}
-                  <p>БЕСПЛАТНО</p>
-                </div>
-                <div className={cls.servicesImg}>
-                  <img src={item.img} />
-                </div>
+          {serviceData.map((item) => (
+            <div key={item.id} className={cls.servicesDelivery}>
+              <div className={cls.servicesName}>
+                <div className={cls.inServicesName}>{item.name}</div>
+                <div className={cls.inServicesTitle}>{item.title}</div>
+                <div className={cls.inServicesP}><p>БЕСПЛАТНО</p></div>
               </div>
-            ))}
-          </div>
+              <div className={cls.servicesImg}><img src={item.img} /></div>
+            </div>
+          ))}
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Services;
