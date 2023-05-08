8d5a9461fa0466c54ed4b006f35384f8ac63f3c0
Faxriddin747 <faxriddinradjapov1@gmail.com>
2023-05-08 18:32:18 +0500

    changes in Hero

--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 
 import cls from "./hero.module.scss";
+// import Tablo from "../components/Tablo";
+
 
 const Hero = () => {
   return (
     <div className="container">
-      <div className={cls.background}>
-        <img src="/BG.png" />
+      <div className={cls.tablo}>
+        <img className={cls.tabloImg} src="/BG.png" />
+        <div className={cls.tabloLeft}>
+          <h1 className={cls.tabloLeftH1}>
+            Доставка товаров из Турции в Узбекистан
+          </h1>
+          <span>
+            Начните экономить до 80% на шопинге. Регистрируйтесь в Modagram, чтобы покупать со скидками в Турции одежду, обувь, гаджеты известных брендов и безопасно отправлять вещи в Узбекистан.
+          </span>
+          <button>
+            Регистрация
+          </button>
+        </div>
+        <div className={cls.tabloRight}>
+          <img src="headerImage.png" />
+        </div>
       </div>
     </div>
   );
