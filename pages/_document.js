c6285fac5a37a7ddd4ae9a43fd0981d9c812ab5a
Rafikjanov Boburjon Botir o'g'li <BRafikzhanov@beeline.uz>
2024-02-03 21:18:13 +0500

    added script

--- pages/_document.js
+++ pages/_document.js
@@ -3,10 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script type="text/javascript"
+                src="https://smartarget.online/loader.js?u=f13dff4c3b65bfd24b4e98ed2f7502fc3439f0df"></script>
+      </Head>
       <body>
-        <Main />
-        <NextScript />
+      <Main/>
+      <NextScript/>
       </body>
     </Html>
   )
